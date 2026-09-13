335d72c48b8d3292363a7160216a3f48f8a98948
DESKTOP-SEQBK09\user <haminyoon12@gmail.com>
2026-09-13 19:18:22 +0900

    Allow grabbing ladder mid-air by pressing up, but prevent grabbing it by pressing down while falling

--- src/player.js
+++ src/player.js
@@ -344,10 +344,10 @@ class Player {
         if (overlappingLadder && !this.isClimbingLadder) {
             // Check if we should attach
             let canAttach = false;
-            if (!this.isTumbling && !this.isGroundPounding && this.isGrounded) {
-                if (keys.actionUp) { canAttach = true; }
-                else if (keys.actionDown) {
-                    if (this.standingOnEntity && this.standingOnEntity.type === 'oneway') { canAttach = true; }
+            if (!this.isTumbling && !this.isGroundPounding) {
+                if (keys.actionUp && this.vy >= 0) { canAttach = true; }
+                else if (keys.actionDown && this.isGrounded && this.standingOnEntity && this.standingOnEntity.type === 'oneway') { 
+                    canAttach = true; 
                 }
             }
             if (canAttach) {
