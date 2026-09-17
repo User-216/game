f19c0db265b59e0d838d357d79467dc32e8988df
DESKTOP-SEQBK09\user <haminyoon12@gmail.com>
2026-09-17 19:51:19 +0900

    Increase spr_player_mach3 animation speed significantly

--- src/player.js
+++ src/player.js
@@ -1294,7 +1294,7 @@ class Player {
             if (this.sprite_index === 'spr_player_mach2') {
                 this.image_speed = 0.25 + (Math.abs(this.vx) * 0.04);
             } else {
-                this.image_speed = 0.2 + (Math.abs(this.vx) * 0.02);
+                this.image_speed = 0.5 + (Math.abs(this.vx) * 0.03); // Faster mach3 animation
             } 
             
             if (this.isGrounded) {
