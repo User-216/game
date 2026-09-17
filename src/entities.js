3007af6af9d975f541e6575fc9121eb296d63386
DESKTOP-SEQBK09\user <haminyoon12@gmail.com>
2026-09-17 23:38:49 +0900

    Set enemy animation speed to match 40ms per frame

--- src/entities.js
+++ src/entities.js
@@ -1,4 +1,4 @@
-
+﻿
 
 class Entity {
     constructor(x, y, width, height, color) {
@@ -519,7 +519,7 @@ class Slime extends Entity {
         
         this.sprites = [];
         this.image_index = 0;
-        this.image_speed = 1/3;
+        this.image_speed = 25/60;
         for (let i = 1; i <= 9; i++) {
             let img = new Image();
             img.src = `spr_slimewalk/spr_slimewalk${i}.png`;
