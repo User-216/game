85f0323775bd20042feb938ebc1c2c40a0abf482
DESKTOP-SEQBK09\user <haminyoon12@gmail.com>
2026-09-17 22:09:12 +0900

    Decrease combo timer by 0.1 per frame instead of 0.5

--- src/game.js
+++ src/game.js
@@ -1681,7 +1681,7 @@ this.entities.push(
         
         // Combo Timer Logic
         if (this.comboTimer > 0) {
-            this.comboTimer -= 0.5;
+            this.comboTimer -= 0.1;
             if (this.comboTimer <= 0) {
                 this.combo = 0;
             }
